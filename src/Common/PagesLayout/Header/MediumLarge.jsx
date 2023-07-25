87548f098c27f09481d9f94bc12d7edc846152a8
Evgeniya Ivanova <evgeniya.ivanova@gmail.com>
2023-07-24 20:09:46 -0700

    S5 Add Hamburger Icon

--- src/Common/PagesLayout/Header/MediumLarge.jsx
+++ src/Common/PagesLayout/Header/MediumLarge.jsx
@@ -9,7 +9,7 @@ const MediumLarge = () => {
     return (
         <MediumLargeStyled className='MediumLarge'>
             <Inset>
-                <div className="inset">
+                <div className="row">
                     <div className="logo">
                         <SiteLogo />
                     </div>
@@ -32,5 +32,58 @@ const MediumLarge = () => {
 export default MediumLarge;
 
 const MediumLargeStyled = styled.div`
+
+.row {
+    display: grid;
+    grid-template-columns: 25% 75%;
+
+    .logo { }
+    .menus {
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-end;
+    }
+}
+
+color: white;
+
+.logo {
+    .SiteLogo {
+        width: 80%;
+        margin: auto;
+        padding: 20px 0px;
+    }
+}
+
+nav.mainMenu {
+    text-align: center;
+        a {
+            width: 80px;
+            display: inline-block;
+
+            line-height: 30px;
+            margin: 0px 0px 20px 5px;
+            border-bottom: solid 3px #84d4d4;
+
+            text-align: center;
+            font-size: 12px;
+            opacity: 0.8;
+            color: #84d4d4;
+            font-weight: bold;
+            text-decoration: none;
+            text-transform: uppercase;
+
+            &:hover,
+            &:active,
+            &:focus {
+                opacity: 1;
+            }
+
+            &.active {
+                color: white;
+                border-bottom-color: white;
+        }
+    }
+}
 
 `;
--- src/Common/PagesLayout/Header/Small.jsx
+++ src/Common/PagesLayout/Header/Small.jsx
@@ -1,10 +1,54 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
+/* Components ---------------------------*/
+import SiteLogo from '../SiteLogo/SiteLogo.jsx';
+import Hamburger from './Hamburger.jsx';
 
 const Small = () => {
+
+    const [isMenuShowing, isMenuShowingUpdate] = useState(false);
+
+    const toggleMenu = () => {
+        isMenuShowingUpdate(!isMenuShowing);
+    };
+
+    const hideMenu = () => {
+        isMenuShowingUpdate(false);
+    };
+
+    useEffect(() => {
+        if (isMenuShowing) {
+            // add class model-open
+            document.body.classList.add('modal-open');
+        } else {
+            // remove class model-open
+            document.body.classList.remove('modal-open');
+        }
+
+        // on dismount clean up
+        return () => {
+            document.body.classList.remove('modal-open');
+        };
+
+    }, [isMenuShowing]);
+
     return (
         <SmallStyled className='Small'>
-            Small
+            <SiteLogo />
+
+            <Hamburger toggleMenu={toggleMenu} />
+
+            {isMenuShowing && (
+                <nav className="mainMenu">
+                    <NavLink onClick={hideMenu} to={'/'} end>
+                        Home
+                    </NavLink>
+                    <NavLink onClick={hideMenu} to={'/staff'}>Staff</NavLink>
+                    <NavLink onClick={hideMenu} to={'/contact'}>Contact</NavLink>
+                    <NavLink onClick={hideMenu} to={'/course-work'}>Course</NavLink>
+                </nav>)}
         </SmallStyled>
     );
 };
@@ -13,4 +57,42 @@ export default Small;
 
 const SmallStyled = styled.div`
     color: white;
+
+    display: flex;
+    justify-content: center;
+
+    .SiteLogo {
+        height: 300px;
+        padding: 20px;
+        max-width: 330px;
+    }
+
+    .mainMenu {
+        position: fixed;
+
+        left: 0px;
+        top: 300px;
+        right: 0px;
+        bottom: 0px;
+
+        background-color: #034444;
+
+        a{
+            display: block;
+
+            padding: 10px;
+            border-bottom: 1px solid #999999;
+
+            background-color: #014445;
+
+            color: white;
+            text-decoration: none;
+
+            &:hover,
+            &:active,
+            &:focus {
+                background-color: #043434;
+            }
+        }
+    }
 `;
