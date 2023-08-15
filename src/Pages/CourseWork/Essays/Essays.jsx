a885255d279ef8ce7f9e2023958982b6cda11e35
Evgeniya Ivanova <evgeniya.ivanova@gmail.com>
2023-08-15 16:17:43 -0700

    S9 Essays Added

--- src/Pages/CourseWork/Essays/Essays.jsx
+++ src/Pages/CourseWork/Essays/Essays.jsx
@@ -1,27 +1,38 @@
 import styled from 'styled-components';
+import EssaysText from './EssaysText';
 
 /* Component ---------------------------*/
 import Essay from './Essay.jsx';
 
 const Essays = () => {
-    return (
-        <EssaysStyled className='Essays'>
-            <h2>Essays</h2>
-            <Essay question='Q1: Explain the difference between HTML, CSS and JavaScript.'>
-                1. This is my answer
-            </Essay>
-            <Essay question='Q2: What is the difference between Git and Github?'>
-                2. This is my answer
-            </Essay>
-            <Essay question='Q3: What is the difference between JQuery and React?'>
-                3. This is my answer
-            </Essay>
-            <Essay question='Q4: What is the difference between a Front-End and Back-End Developer?'>
-                4. This is my answer
-            </Essay>
+	const essayAnswer = EssaysText;
+	return (
+		<EssaysStyled className='Essays'>
+			<h2>Essays</h2>
+			<Essay question='Q1: Explain the difference between HTML, CSS and JavaScript.'>
+				{essayAnswer[0]}
+			</Essay>
+			<Essay question='Q2: What is the difference between Git and Github?'>
+				{essayAnswer[1]}
+			</Essay>
+			<Essay question='Q3: What is the difference between JQuery and React?'>
+				{essayAnswer[2]}
+			</Essay>
+			<Essay question='Q4: What is the difference between a Front-End and Back-End Developer?'>
+				{essayAnswer[3]}
+			</Essay>
+			<Essay question='Q5: What is the difference between Absolute, Root and Document Relative linking?'>
+				{essayAnswer[4]}
+			</Essay>
+			<Essay question='Q6: What is the difference between jpg, gif, png and svg images?'>
+				{essayAnswer[5]}
+			</Essay>
+			<Essay question='Q7: Define the following Agile roles: Product Owner, Product Manager, Business Analyst, Scrum Master, UX designer, Developer/Engineer, QA/SDET, and DevOps.'>
+				{essayAnswer[6]}
+			</Essay>
 
-        </EssaysStyled>
-    );
+		</EssaysStyled>
+	);
 };
 
 export default Essays;
