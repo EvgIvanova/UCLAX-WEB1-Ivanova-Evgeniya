3ab63539165e67311a5c3a6bd0fbe4cece82a1ae
Evgeniya Ivanova <evgeniya.ivanova@gmail.com>
2023-08-16 12:26:30 -0700

    S10 Essays Formatted

--- src/Pages/CourseWork/Essays/Essay.jsx
+++ src/Pages/CourseWork/Essays/Essay.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const Essay = ({ question, children }) => {
-    return (
-        <EssayStyled className='Essay'>
-            <h3>{question}</h3>
-            <div className="answer">
-                {children}
-            </div>
-        </EssayStyled>
-    );
+	return (
+		<EssayStyled className='Essay'>
+			<h3>{question}</h3>
+			<div className="answer">
+				{children}
+			</div>
+		</EssayStyled>
+	);
 };
 
 export default Essay;
@@ -27,6 +27,12 @@ const EssayStyled = styled.div`
 
     .answer {
         background-color: #eee;
-        padding: 20px;
+        padding: 20px 20px 10px 20px;
+
+		p {
+			margin: 5px;
+			padding: 0px;
+		}
+
     }
 `;
--- src/Pages/CourseWork/Essays/Essays.jsx
+++ src/Pages/CourseWork/Essays/Essays.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import EssaysText from './EssaysText';
 
 /* Component ---------------------------*/
@@ -10,25 +11,25 @@ const Essays = () => {
 		<EssaysStyled className='Essays'>
 			<h2>Essays</h2>
 			<Essay question='Q1: Explain the difference between HTML, CSS and JavaScript.'>
-				{essayAnswer[0]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[0] }} />
 			</Essay>
 			<Essay question='Q2: What is the difference between Git and Github?'>
-				{essayAnswer[1]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[1] }} />
 			</Essay>
 			<Essay question='Q3: What is the difference between JQuery and React?'>
-				{essayAnswer[2]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[2] }} />
 			</Essay>
 			<Essay question='Q4: What is the difference between a Front-End and Back-End Developer?'>
-				{essayAnswer[3]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[3] }} />
 			</Essay>
 			<Essay question='Q5: What is the difference between Absolute, Root and Document Relative linking?'>
-				{essayAnswer[4]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[4] }} />
 			</Essay>
 			<Essay question='Q6: What is the difference between jpg, gif, png and svg images?'>
-				{essayAnswer[5]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[5] }} />
 			</Essay>
 			<Essay question='Q7: Define the following Agile roles: Product Owner, Product Manager, Business Analyst, Scrum Master, UX designer, Developer/Engineer, QA/SDET, and DevOps.'>
-				{essayAnswer[6]}
+				<div dangerouslySetInnerHTML={{ __html: essayAnswer[6] }} />
 			</Essay>
 
 		</EssaysStyled>
